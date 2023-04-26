35a53e0e94aea0c00db94f9c014bdf7683e1b3a8
nada-cmd <n192004a@gmail.com>
2023-04-26 13:16:39 -0700

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -16,7 +16,7 @@ int flags, int width, int precision, int size)
 {
 int i, unknow_len = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
-{'c', Print_Char}, {'s', print_string}, {'%', print_percent},
+{'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
 {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
