3eeb6833b9b63cb19f8fe16288a856a4e49b1f48
nada-cmd <n192004a@gmail.com>
2023-04-26 13:33:40 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -11,7 +11,7 @@
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+int flags, int width, int precision, int size);
 {
 char c = va_arg(types, int);
 return (handle_write_char(c, buffer, flags, width, precision, size));
